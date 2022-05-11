fa97b43349057d89ad1e06fc0a893ab790a56054
Durgeshwar Sahani <rd113760@gmail.com>
2022-05-12 00:54:53 +0530

    day2complete

---
+++ lib/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final int days = 30;
+  final double count = 3;
+  final bool genderMale = true;
+  final String s = "Durgeshwar";
+  var day = "hero"; //var understand the datatype after defining anytype
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("FlutterLearn"), //app name
+        ),
+        body: Center(
+          child: Container(
+            child: Text(
+                " $s , Hi there am here for $days and $count , His gender is male: $genderMale. He is checking var : $day"),
+          ),
+        ));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:learningapp/home_page.dart';
+import 'home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,12 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-          child: Center(
-        child: Container(
-          child: Text("Hi there am here"),
-        ),
-      )),
+      home: HomePage(),
     );
   }
 }
